af103979c9b1f68c540ba9f960d669967b1cbeda
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-12-01 11:48:25 +0200

    Update ExtractFunction

---
+++ ExtractFunction/StartPoint/StartPoint/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StartPoint
+{
+
+    class LimitsOfArray
+    {
+        public void GetMaxAndMinFromArray(int length, int[] array, ref int max, ref int min)
+        {
+
+            max = array[0];
+            min = array[0];
+            
+            for (int iterator = 0; iterator < length; iterator++)
+            {
+                if (max < array[iterator])
+                    max = array[iterator];
+
+                if (min > array[iterator])
+                    min = array[iterator];
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            LimitsOfArray limits = new LimitsOfArray();
+            
+            int max = 0;
+            int min = 0;
+
+            limits.GetMaxAndMinFromArray(5, new int[] {1, 2, 3, 4, 5}, ref max, ref min);
+            
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+}
