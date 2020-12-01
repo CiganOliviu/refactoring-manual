50e9165d84f1d77b044f46bacc939fc213331e1f
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-12-01 13:54:00 +0200

    Create InlineVariable

---
+++ InlineVariable/StartPoint/StartPoint/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace StartPoint
+{
+    class OneDimensionalArray
+    {
+        private int _length;
+        private readonly int[] _array = {};
+
+        public void SetLength(int length)
+        {
+            this._length = length;
+        }
+        
+        public int GetLength()
+        {
+            return this._length;
+        }
+
+        public int[] GetArray()
+        {
+            return this._array;
+        }
+    }
+
+    class ValidatorObject
+    {
+        public void ValidateLengthOfArray(OneDimensionalArray array)
+        {
+            int length = array.GetLength();
+
+            if (length < 0)
+                throw new InvalidDataException("Unable to process negative length for array");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pass
+        }
+    }
+}
