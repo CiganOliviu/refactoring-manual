978325e5bd2ebf2823f5ad0819a0eb09b7c0d0bf
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-12-01 20:00:34 +0200

    Create EncapsulateVariable

---
+++ EncapsulateVariable/EndPoint/EndPoint/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EndPoint
+{
+    class OneDimensionalArray
+    {
+        private int length;
+        private int[] _oneDimensionalArray = { };
+
+        public void SetLength(int newLength)
+        {
+            this.length = newLength;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+
+        public int[] GetOneDimensionalArray()
+        {
+            return this._oneDimensionalArray;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OneDimensionalArray array = new OneDimensionalArray();
+            
+            array.SetLength(5);
+            
+            // array.ReadOneDimensionalArray(array.GetLength());
+        }
+    }
+}
---
+++ EncapsulateVariable/StartPoint/StartPoint/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StartPoint
+{
+    class OneDimensionalArray
+    {
+        public int length;
+        public int[] oneDimensionalArray = {};
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OneDimensionalArray array = new OneDimensionalArray();
+            array.length = 5;
+
+            // array = ReadOneDimensionalArray(array.length);
+        }
+    }
+}
