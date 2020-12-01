5a3384ba8a2d2b2bfbf107821e847d0a1f6345a7
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-12-01 13:31:17 +0200

    Create ExtractVariable

---
+++ ExtractVariable/EndPoint/EndPoint/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace EndPoint
+{
+    class NumberProcessor
+    {
+        public int ReverseNumber(int number)
+        {
+            int result = 0;
+
+            while (number > 0)
+            { 
+                int lastDigit = number % 10;
+                result = result * 10 + lastDigit;
+                number /= 10;
+            }
+
+            return result;
+        }    
+    }
+
+    class SystemTests
+    {
+        private readonly NumberProcessor _processor;
+        public SystemTests()
+        {
+            _processor = new NumberProcessor();    
+        }
+        
+        private static bool Assert(bool statement)
+        {
+            if (statement == false)
+                throw new ArgumentException("Assertion failed");
+
+            return true;
+        }
+
+        public void TestReverseNumber()
+        {
+            Assert(_processor.ReverseNumber(1234) == 4321);
+            Assert(_processor.ReverseNumber(1203) == 3021);
+            Assert(_processor.ReverseNumber(9898) == 8989);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemTests tests = new SystemTests();
+            
+            tests.TestReverseNumber();
+        }
+    }
+}
---
+++ ExtractVariable/StartPoint/StartPoint/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace StartPoint
+{
+    class NumberProcessor
+    {
+        public int ReverseNumber(int number)
+        {
+            int result = 0;
+            
+            while (number > 0)
+            {
+                result = result * 10 + number % 10;
+                number /= 10;
+            }
+
+            return result;
+        }    
+    }
+
+    class SystemTests
+    {
+        private readonly NumberProcessor _processor;
+        public SystemTests()
+        {
+            _processor = new NumberProcessor();    
+        }
+        
+        private static bool Assert(bool statement)
+        {
+            if (statement == false)
+                throw new ArgumentException("Assertion failed");
+
+            return true;
+        }
+
+        public void TestReverseNumber()
+        {
+            Assert(_processor.ReverseNumber(1234) == 4321);
+            Assert(_processor.ReverseNumber(1203) == 3021);
+            Assert(_processor.ReverseNumber(9898) == 8989);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemTests tests = new SystemTests();
+            
+            tests.TestReverseNumber();
+        }
+    }
+}
