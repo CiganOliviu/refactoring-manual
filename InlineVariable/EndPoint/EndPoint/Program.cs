6a7825175f7fe9db671a8fe7a615cf6b2329f507
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-12-01 13:58:45 +0200

    Update InlineVariable with EndPoint

---
+++ InlineVariable/EndPoint/EndPoint/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace StartPoint
+{
+    class OneDimensionalArray
+    {
+        private int _length;
+        private readonly int[] _array = {};
+
+        public void SetLength(int length)
+        {
+            this._length = length;
+        }
+        
+        public int GetLength()
+        {
+            return this._length;
+        }
+
+        public int[] GetArray()
+        {
+            return this._array;
+        }
+    }
+
+    class ValidatorObject
+    {
+        public void ValidateLengthOfArray(OneDimensionalArray array)
+        {
+            if (array.GetLength() < 0)
+                throw new InvalidDataException("Unable to process negative length for array");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pass
+        }
+    }
+}
