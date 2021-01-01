73e4f4189e50d76c831e2f2d543d10d2995f5f30
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2021-01-01 21:14:19 +0200

    Update refactoring-manual

---
+++ EncapsulateRecord/EndPoint/EndPoint/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EndPoint
+{
+
+    class Organization
+    {
+        private string _name;
+        private string _country;
+
+        public void SetName(string name)
+        {
+            this._name = name;
+        }
+
+        public string GetName()
+        {
+            return this._name;
+        }
+
+        public void SetCountry(string country)
+        {
+            this._country = country;
+        }
+
+        public string GetCountry()
+        {
+            return this._country;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Organization organization = new Organization();
+            
+            organization.SetName("Cognizant Softvision");
+            organization.SetCountry("Romania");
+        }
+    }
+}
---
+++ EncapsulateRecord/StartPoint/StartPoint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StartPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> organization = new Dictionary<string, string>();
+            
+            organization.Add("Cognizant Softvision", "Romania");
+        }
+    }
+}
