63b387f371960aa5272f6e921645cec797a3a02e
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2021-01-01 21:45:25 +0200

    Update refactoring-manual with EncapsulateCollection

---
+++ EncapsulateCollection/EndPoint/EndPoint/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace EndPoint
+{
+    class OneDimensionalArray
+    {
+        private int[] _array;
+        
+        private int[] GetSliceOf(int[] array)
+        {
+            int[] result = new int[array.Length];
+
+            for (int it = 0; it < array.Length; it++)
+                result[it] = array[it];
+            
+            return result;
+        }
+        
+        public int[] GetOneDimensionalArray()
+        {
+            return GetSliceOf(this._array);
+        }
+        
+        public void SetElementOfOneDimensionalArray(int value, int position)
+        {
+            this._array[position] = value;
+        }
+
+        public int GetElementOfOneDimensionalArray(int position)
+        {
+            return this._array[position];
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EncapsulateCollection/StartPoint/StartPoint/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StartPoint
+{
+    class OneDimensionalArray
+    {
+        private int[] _array;
+
+        public void SetOneDimensionalArray(int[] array)
+        { 
+            this._array = array;
+        }
+        
+        public int[] GetOneDimensionalArray()
+        {
+            return this._array;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
